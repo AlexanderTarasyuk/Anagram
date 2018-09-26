c6e50ca52dff5fbb6df7edc80d9d68c695bf7e56
AlexandrTarasyuk <alextarasyuk79@gmail.com>
2018-09-26 10:03:31 +0300

    refactoring logic, view , rotating

--- app/src/main/java/com/example/alextarasyuk/anagram/MainActivity.java
+++ app/src/main/java/com/example/alextarasyuk/anagram/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.alextarasyuk.anagram;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.alextarasyuk.anagram.util.AnagramConverter;
+import com.example.alextarasyuk.anagram.util.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,13 +30,19 @@ public class MainActivity extends AppCompatActivity {
     private String LOG = this.getClass().getSimpleName();
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        if (savedInstanceState!=null){
+
+            mTvResult.setText(savedInstanceState.getString(Constants.resultTextState));
+        }
 
         mButtonConvert.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
                 Log.d(LOG, "Convert text");
@@ -42,13 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
 
                 } else {
-                    mTvResult.setText("Initial text is:" + mEdtInput.getText().toString() +
-                            "/n" + AnagramConverter.convertToAnagram(mEdtInput.getText().toString()));
+                    mTvResult.setText("Initial text is: " + mEdtInput.getText().toString() +
+                            "\n  Anagram is:  " + AnagramConverter.convertLineToAnagram(mEdtInput.getText().toString()));
                 }
                 mEdtInput.setText("");
 
             }
         });
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(Constants.resultTextState, mTvResult.getText().toString());
     }
 }
--- app/src/main/java/com/example/alextarasyuk/anagram/util/AnagramConverter.java
+++ app/src/main/java/com/example/alextarasyuk/anagram/util/AnagramConverter.java
@@ -1,11 +1,28 @@
 package com.example.alextarasyuk.anagram.util;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
 public class AnagramConverter {
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String convertLineToAnagram(String lineToAnagram) {
+        String[] stringLineToAnagram = lineToAnagram.split(" ");
+        StringBuilder stringBuilder=new StringBuilder();
+        for (String string: stringLineToAnagram) {
+            stringBuilder.append(convertToAnagram(string));
+            stringBuilder.append(" ");
+        }
+        return stringBuilder.toString();
+    }
 
 
     //this method reverse all the words of input text.
     // All non-letter symbols should stay on the same places
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static String convertToAnagram(String stringToReverse) {
+
+
         char[] arrayString = stringToReverse.toCharArray();
         int i = 0;
         int j = arrayString.length - 1;

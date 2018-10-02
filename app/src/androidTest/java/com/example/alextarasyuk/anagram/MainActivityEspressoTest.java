a6d92f569775f72f9d5f916dd20f4c551cce4bb4
AlexandrTarasyuk <alextarasyuk79@gmail.com>
2018-10-02 09:07:00 +0300

    spoon and espresso tests are added

---
+++ app/src/androidTest/java/com/example/alextarasyuk/anagram/MainActivityEspressoTest.java
@@ -0,0 +1,99 @@
+package com.example.alextarasyuk.anagram;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureTextFromInputTextIsConvertedToAnagramToTextViewResultAfterButtonConvertIsClicked() {
+
+        onView(withId(R.id.edt_input_text))
+                .perform(typeText("asdf")
+                        , closeSoftKeyboard());
+
+        onView(withId(R.id.btn_convert))
+                .perform(click());
+
+        onView(withId(R.id.tv_result))
+                .check(matches(withText("Initial text is: " + "asdf" +
+                        "\n  Anagram is:  " + "fdsa")));
+    }
+
+    @Test
+    public void ensureTextFromInputTextIfIsEmptyToastWillApearAfterButtonConvertIsClicked() {
+
+        onView(withId(R.id.edt_input_text))
+                .perform(typeText("")
+                        , closeSoftKeyboard());
+
+        onView(withId(R.id.btn_convert))
+                .perform(click());
+
+        onView(withText(startsWith("Please"))).
+                inRoot(withDecorView(
+                        not(is(mainActivityActivityTestRule.getActivity().
+                                getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ensureEditTextIsCleanAfterButtonConvertIsClicked() {
+
+        onView(withId(R.id.btn_convert))
+                .perform(click());
+        onView(withId(R.id.edt_input_text))
+                .check(matches(withText("")));
+    }
+
+    @Test
+    public void ensureTextVievStateIsTheSameAfterChangingTheOrientation() {
+        onView(withId(R.id.tv_result))
+                .perform(typeText("asdf"));
+
+        rotateScreen();
+
+        onView(withId(R.id.tv_result))
+                .check(matches(withText("asdf")));
+
+    }
+
+    private void rotateScreen() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        int orientation
+                = context.getResources().getConfiguration().orientation;
+
+        Activity activity = mainActivityActivityTestRule.getActivity();
+        activity.setRequestedOrientation(
+                (orientation == Configuration.ORIENTATION_PORTRAIT) ?
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE :
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
--- app/src/test/java/com/example/alextarasyuk/anagram/util/AnagramConverterTest.java
+++ app/src/test/java/com/example/alextarasyuk/anagram/util/AnagramConverterTest.java
@@ -1,15 +1,20 @@
 package com.example.alextarasyuk.anagram.util;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AnagramConverterTest {
 
     @Test
-    public void givenStringShouldOutputAnagram() {
+    public void givenStringLineShouldOutputAnagram() {
         assertEquals(AnagramConverter.convertLineToAnagram("a1bcd efg!h"), "d1cba hgf!e");
     }
+
+    @Test
+    public void givenStringShouldOutputAnagram() {
+        assertEquals(AnagramConverter.convertToAnagram("a1bcd"), "d1cba");
+    }
+
+
 }
